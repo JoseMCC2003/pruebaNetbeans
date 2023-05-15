a6d3938a1fbe70628f9fed77a0275bed8cf1c81f
José Manuel <Jos Manuel>
2023-05-15 14:27:21 +0200

    proyecto

--- src/proyectoprueba/ProyectoPrueba.java
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -18,6 +18,7 @@ public class ProyectoPrueba {
         // TODO code application logic here
         System.out.println("Hola");
         System.out.println("Adios");
+        System.out.println("Hasta Lueog");
     }
     
 }
