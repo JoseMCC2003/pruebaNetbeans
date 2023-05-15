e34e91a738728ed7a8c6bcb5b44b0e8d0f621179
José Manuel <Jos Manuel>
2023-05-15 13:57:15 +0200

    proyecto

---
+++ src/hola/fdlkmg.java
@@ -0,0 +1,24 @@
+package hola;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JoséManuelCastilloCa
+ */
+public class fdlkmg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+        System.out.println("Aprendiendo git en netbeans");
+    }
+    
+}
